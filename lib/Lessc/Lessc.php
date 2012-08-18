204a50eeb85e4e3629812434591220baed9fc2f8
Quan <info@brodev.com>
2012-08-18 17:04:51 +0700

    Add Lessc class to compatible with Symfony2

---
+++ lib/Lessc/Lessc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Author: Truong Manh Quan
+ * Copyright: Brodev Software - www.brodev.com
+ */
+
+require_once __DIR__ . '/../../lessc.inc.php';
+
+class Lessc_Lessc extends lessc
+{
+
+}
